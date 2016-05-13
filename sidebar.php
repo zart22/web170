8acc188c80e0149bdde35cdacf1f7b43679c5b67
zart22 <zbirja01@seattlecentral.edu>
2016-05-13 15:10:14 -0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+
+    
+<aside id="map">
+<h3>Map & Direction</h3>
+<p> 3326 lake Sammamish Pkwy,
+Sammamish, WA 98074</p>
+<div class='embed-container'><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.5066983845627!2d-122.32370048433502!3d47.61628099530236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906accc351c149%3A0xdc1a5c338dd4395c!2sSeattle+Central+College!5e0!3m2!1sen!2sus!4v1447821413541"></iframe></div>
+    </aside>
+     </div>
+    
+<!-- END Aside -->
